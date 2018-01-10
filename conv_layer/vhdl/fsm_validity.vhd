d373939efb52685aec2d7b84e306faf89a07a4aa
Jakub Hrabovsky <jakhrabovsky@gmail.com>
2018-01-10 13:04:33 +0100

    Updated: corrected startup delay and horizontal border transition

--- conv_layer/vhdl/fsm_validity.vhd
+++ conv_layer/vhdl/fsm_validity.vhd
@@ -3,8 +3,6 @@ library IEEE;
     use IEEE.STD_LOGIC_1164.ALL;
     use ieee.numeric_std.all;
 
-use work.misc_pkg.all;
-
 entity fsm is
     Generic (
         INPUT_ROW_LENGTH : integer := 9;
@@ -35,9 +33,30 @@ function log2c (N : integer) return integer is
         return m;
     end log2c;
 
-constant STARTUP_DELAY_PART1 : integer := KERNEL_SIZE * (2 * KERNEL_SIZE - 1) + 2; -- +2 = 1xOUTPUT-REG and 1xREG in the last DSP (between multilpier & adder) 
-constant STARTUP_DELAY_PART2 : integer := (KERNEL_SIZE - 1) * (INPUT_ROW_LENGTH - 2 * KERNEL_SIZE + 1) + 1; -- +1 = OUTPUT-REG
-constant STARTUP_DELAY : integer := STARTUP_DELAY_PART1 + STARTUP_DELAY_PART2; 
+-------------------------------            
+--      STARTUP-DELAY        --
+-------------------------------
+
+-- [!] need to distinguish two cases:
+--      (A) when [N-2K+1 >= 0] then the startup-delay is sum of delays of part1 and part2.
+--      (B) when [N-2K+1 < 0] then the data-path in the part2 is reversed ==> the startup delay consists only of delay of part1.
+
+-- constant STARTUP_DELAY_PART1 : integer := KERNEL_SIZE * (2 * KERNEL_SIZE - 1) + 2; -- +2 = 2xREG in the last DSP (between multilpier & adder and at output) 
+-- constant STARTUP_DELAY_PART2 : integer := (KERNEL_SIZE - 1) * (INPUT_ROW_LENGTH - 2 * KERNEL_SIZE + 1); 
+-- constant STARTUP_DELAY_A : integer := STARTUP_DELAY_PART1 + STARTUP_DELAY_PART2 + 1; -- +1 = OUTPUT-REG
+-- constant STARTUP_DELAY_B : integer := STARTUP_DELAY_PART1 + 1;
+
+function startup_delay (N, K : integer) return integer is
+variable part1, part2 : integer; 
+begin
+    part1 := K * (2*K - 1) + 2;
+    part2 := (K-1) * (N - 2*K + 1);
+    if (N - 2*K + 1 >= 0) then
+        return part1 + part2 + 1;
+    else
+        return part1 + 1;
+    end if;
+end startup_delay;
 
 constant NO_INVALID_PIXELS_PER_LINE : integer := KERNEL_SIZE - 1; -- number of invalid pixels per line (vertical border crossing)
 constant NO_VALID_PIXELS_PER_LINE : integer := INPUT_ROW_LENGTH - KERNEL_SIZE + 1; -- number of valid pixels per line (inside image)
@@ -67,7 +86,7 @@ signal valid_out_tmp : std_logic;
 --      PIXEL-COUNTER PARAMS --
 -------------------------------
 
-constant PIXEL_COUNTER_THRESHOLD_WIDTH : natural := log2c(STARTUP_DELAY); -- PIXEL_COUNTER uses various thresholds during processing => STARTUP_DELAY, NO_VALID_PIXELS_PER_LINE, and NO_INVALID_PIXELS_PER_LINE.
+constant PIXEL_COUNTER_THRESHOLD_WIDTH : natural := log2c(startup_delay(INPUT_ROW_LENGTH, KERNEL_SIZE)); -- PIXEL_COUNTER uses various thresholds during processing => STARTUP_DELAY, NO_VALID_PIXELS_PER_LINE, and NO_INVALID_PIXELS_PER_LINE.
                                                        -- because of the required bit-width the maximum of the mentioned values has to be assumed => considering their structure, the STARTUP_DELAY should be always the maximum of them.
 signal pixel_counter_clear, pixel_counter_set, pixel_counter_alert, pixel_counter_ce : std_logic;
 signal pixel_counter_clear_tmp, pixel_counter_set_tmp: std_logic;
@@ -86,8 +105,8 @@ constant NO_VALID_LINES_PER_IMAGE : natural := INPUT_ROW_LENGTH - KERNEL_SIZE + 
 -- number of PIXELS per vertical border transition
 constant NO_INVALID_PIXELS_PER_TRANSITION : natural := (KERNEL_SIZE - 1) * INPUT_ROW_LENGTH - 1;
 -- the FSM waits for 1 CLK more in inside_image state in order to identify transition to horizontal_border
-constant LINE_COUNTER_THRESHOLD_WIDTH : natural := log2c(NO_VALID_LINES_PER_IMAGE);
--- LINE_COUNTER uses only NO_VALID_LINES_PER_IMAGE as a threshold. 
+constant LINE_COUNTER_THRESHOLD_WIDTH : natural := log2c(NO_VALID_LINES_PER_IMAGE) + 2; -- need 2 more bits because in the case [N - 2K + 1 < 0] the original computed width is -1. 
+-- LINE_COUNTER uses only NO_VALID_LINES_PER_IMAGE as a threshold.
 
 signal line_counter_threshold : std_logic_vector(LINE_COUNTER_THRESHOLD_WIDTH - 1 downto 0);
 begin
@@ -145,8 +164,8 @@ begin
 		
         case state_reg is
             when init =>
-                pixel_counter_threshold <= std_logic_vector(to_unsigned(STARTUP_DELAY - 1, PIXEL_COUNTER_THRESHOLD_WIDTH));
-		      	pixel_counter_set_tmp <= '1';
+                pixel_counter_threshold <= std_logic_vector(to_unsigned(startup_delay(INPUT_ROW_LENGTH, KERNEL_SIZE) - 1, PIXEL_COUNTER_THRESHOLD_WIDTH));
+                pixel_counter_set_tmp <= '1';
                 state_next <= start_up;
                 
             when start_up =>
